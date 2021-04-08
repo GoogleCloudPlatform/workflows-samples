72fea2bff863a2dbf6a6fc71b3312c584ddf4bdc
Grant Timmerman <timmerman+devrel@google.com>
2021-04-08 15:31:54 -0500

    ci: improve script
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- validateFilenames.sh
+++ validateFilenames.sh
@@ -3,10 +3,16 @@
 echo "Validating filenames..."
 workflowsFilePattern="*.workflows.yaml";
 
+# Check all workflow files to see if the file pattern is good.
 cd src
 for path in $(find . -name '*.yaml'); do
-  [[ $path == $workflowsFilePattern ]] &&
-    echo "File is fine!" ||
-    echo "$path doesn't match *.workflows.yaml" &&
-    exit
+  if [[ $path == $workflowsFilePattern ]]; then
+    echo "$path is fine!"
+  else
+    echo "$path doesn't match *.workflows.yaml"
+    FAILED=true
+  fi
 done
+
+# Fail CI if a path doesn't match.
+if [[ $FAILED ]]; then exit 1;
