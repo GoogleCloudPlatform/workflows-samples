c20a3452499983cfa047a98d45b4dd6df452e718
Grant Timmerman <timmerman+devrel@google.com>
2021-04-08 15:15:56 -0500

    ci: validate filenames
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

---
+++ validateFilenames.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+workflowsFilePattern="*.workflows.yaml";
+
+cd src
+for path in $(find . -name '*.yaml'); do
+  [[ $path == $workflowsFilePattern ]] &&
+    echo "File is fine!" ||
+    echo "$path doesn't match *.workflows.yaml" &&
+    exit
+done
