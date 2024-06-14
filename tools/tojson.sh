88ca7ed7feadff343667850af7039263979b4149
Jindong Che <jindongc@google.com>
2024-06-14 12:39:31 -0400

    - Update the tojson script to allow taking one file input. Rename connector_vertexai_workflows.yaml to connector_aiplatform_workflows.yaml to match name in its discovery doc: https://aiplatform.googleapis.com/$discovery/rest?version=v1

--- tools/tojson.sh
+++ tools/tojson.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-# Copyright 2023 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,12 @@
 # Requires `npm install -g yamljs`
 # https://www.npmjs.com/package/yamljs
 
+echo "Please enter the yaml file name you want to convert to json and include a relative path from root."
+echo "Example: src/http_get.workflows.yaml"
+echo "Enter 'all' if you want to convert all yaml files."
+read -p 'File: ' file
+file=$(echo "$file" | tr '[:upper:]' '[:lower:]')
+
 if ! command -v yaml2json &> /dev/null
 then
     echo "ERROR: yaml2json could not be found"
@@ -28,9 +34,15 @@ then
 fi
 
 # Loop through all yaml files, convert to json
-cd src
-for path in $(find . -name '*.yaml'); do
-  yaml2json $path --save --pretty
-done
-cd ..
+if [[ $file =  "all" ]]
+then
+  cd src
+  for path in $(find . -name '*.yaml'); do
+    yaml2json $path --save --pretty
+  done
+  cd ..
+else
+  yaml2json $file --save --pretty
+fi
+
 echo 'Done!'
